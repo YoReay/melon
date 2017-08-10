ea73e0f568c06d4793f7884974048d7c44b99659
YoReay <Yr492107643@Gmail.com>
2017-08-10 15:59:31 +0800

    新增服务器端

---
+++ melon-server/src/main/java/com/yoreay/melon/server/MelonServerApplication.java
@@ -0,0 +1,12 @@
+package com.yoreay.melon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelonServerApplication.class, args);
+	}
+}
